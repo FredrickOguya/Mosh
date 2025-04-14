64b3c586ca1b858114e5c6068460d5e5195205f0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-14 15:06:46 +0300

    My solution

---
+++ app.py
@@ -0,0 +1,16 @@
+command = ""
+
+while command.lower() != "stop":
+    command.lower()= input(">")
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car stopped")
+    elif command == "help":
+        print("""
+              start - to start the car
+              stop - to stop the car
+              quit - to quit
+            """)
+    else:
+        "Sorry I don't Understand the command"
