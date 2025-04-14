66297dd922310935c8c6f102e3cfc485ddd77952
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-15 00:39:46 +0300

    largest number

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
-names[0]= 'Fred'
-print(names)
+numbers = [2,4,5,4,7,8,9,2,1,11,23,1,0]
+
+largest = 0
+for number in numbers:
+    if number > largest:
+        largest = number
+print(f"largest number is {largest}")
