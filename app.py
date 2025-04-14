937901c8c4b6e3d403e993f4696b0b3635b8f261
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-14 18:09:34 +0300

    for loop

--- app.py
+++ app.py
@@ -1,19 +1,2 @@
-command = ""
-
-
-while True:
-    command = input(">").lower()
-    if command == "start":
-        print("Car started...")
-    elif command == "stop":
-        print("Car stopped")
-    elif command == "help":
-        print("""
-              start - to start the car
-              stop - to stop the car
-              quit - to quit
-            """)
-    elif command == "quit":
-        break
-    else:
-        print("Sorry I don't Understand the command")
+for item in range(5, 10,2):
+    print(item)
