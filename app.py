72e7d5506c7bf41787b334a164e5a4fe9dcbc6f3
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-16 06:00:00 +0300

    Classes

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-# comment
+class Point:
+    def move(self):
+        print("move")
+    
+    def draw(self):
+        print("draw")
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+
+
+point2 = Point()
+print(point1.x)
+point1.draw()
