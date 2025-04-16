f435ba38921e58bfb56a6d6ac9518e8e69262c3e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-16 06:21:00 +0300

    Comments

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-try:
-    age = int(input('Age: '))
-    income = 20000
-    risk = income / age
-    print(age)
-except ZeroDivisionError:
-    print("age cannot be 0.")
-except ValueError:
-    print('Invalid value')
+# comment
