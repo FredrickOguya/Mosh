8a0817768755cc6cce02699f6ebebae19a3f9b56
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-16 06:16:00 +0300

    Return Statement

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-def greet_user(first_name,last_name):
-    print(f'Hi {first_name} {last_name}!')
-    print('Welcome aboard')
+def square(number):
+    print(number * number) 
 
-print("Start")
-greet_user(last_name="Smith",first_name="John")
-
-
-print("Finish")
+print(square(3))
