7ae121f6b86e1cc468714136c7b0f938be53eef4
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-16 06:10:00 +0300

    Constructor

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
     def move(self):
         print("move")
     
@@ -6,11 +9,7 @@ class Point:
         print("draw")
 
 
-point1 = Point()
-point1.x = 10
-point1.y = 20
+point = Point(10,20)
+point.x=11
+print(point.x)
 
-
-point2 = Point()
-print(point1.x)
-point1.draw()
