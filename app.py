b9c030ef509466af633a8e0bd93f7e63236c02f8
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-17 11:09:20 +0300

    Excel Spreadsheet

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from pathlib import Path
+import openpyxl as xl 
+from openpyxl.chart import BarChart, Reference
 
-path = Path()
 
-#print(path.exists())
-#print(path.mkdir())
-#print(path.rmdir())
-for file in path.glob('* '):
-    print(file)
+def process_workbook(filename):
+    wb = xl.load_workbook('transactions.xlsx')
+    sheet = wb['Sheet1']
+
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row,3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row,4)
+        corrected_price_cell.value = corrected_price
+
+
+    values = Reference(sheet, 
+            min_row=2, 
+            max_row = sheet.max_row,
+            min_col=4,
+            max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
