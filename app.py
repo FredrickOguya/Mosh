6e8f95b33a905cb1d240deac8a6665357bc2b1f7
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-18 06:28:40 +0300

    **args

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-def multiply(*numbers):
-    total = 1
-    for number in numbers:
-        total*=number
-    return total
+def save_user(**user):
+    print(user["name"])
 
-
-print(multiply(2,3,4, 5))
+save_user(id=1,name="fred",age="35")
