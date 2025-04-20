74d03d757f799aba774f9dca6a1795000426c23a
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-20 06:15:49 +0300

    Finding Items

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 letters = ["a", "b", "c"]
-
-#add
-letters.append("d")
-letters.insert(0,"-")
-
-#Remove
-letters.pop(0)
-letters.remove("b")
-del letters[0:3]
-letters.clear()
-print(letters)
+print(letters.count("b"))
+if "d" in letters:
+    print(letters.index("a"))
