fcafa70f8026a6303dff6e486279ed18d7f50cd7
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-20 06:15:49 +0300

    Sorting Lists

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-letters = ["a", "b", "c"]
-print(letters.count("b"))
-if "d" in letters:
-    print(letters.index("a"))
+# numbers = [3, 51, 2, 8, 6]
+# # numbers.sort(reverse=True)
+# print(sorted(numbers, reverse=True))
+# print(numbers)
+
+items = [
+    ("product1", 10),
+    ("product2", 9),
+    ("product3", 12)
+]
+
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)                                                                                              
+print(items)
