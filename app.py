52f123e91e36d7a70cd1e6e648fc8ffe48408d3d
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-20 06:12:49 +0300

    Looping over lists

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-numbers = [1,2,3,4,5,6,8,9,7]
+letters = ["a", "b", "c"]
 
-# first, second , third = numbers
-# first, second , *other = numbers
-
-first, *other , last = numbers
-
-print(first)
-print(other)
-print(last)
-
-    
+for index,letter in enumerate(letters):
+    print(index,letter)
