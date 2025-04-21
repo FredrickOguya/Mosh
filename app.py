fd31658032be2a98f36e768408aee93be46429cb
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-21 06:17:20 +0300

    Queues

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-browsing_session = []
-browsing_session.append(1)
-browsing_session.append(2)
-browsing_session.append(3)
-print(browsing_session)
-last = browsing_session.pop()
-print(last)
-print(browsing_session)
-print("redirect", browsing_session[-1])
-if not browsing_session:
-    print("disable")
+from collections import deque
+
+queue = deque([])
 
 
+queue.append(1)
+queue.append(2)
+queue.append(3)
+queue.popleft()
+print(queue)
+if not queue:
+    print("empty")
