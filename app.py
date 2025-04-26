d8940a9402758e9f0275b467a6bd2961a16bddcb
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-26 08:27:50 +0300

    Cleaning Up

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-# numbers = [1,2]
-
-# print(numbers[3])
 
 
 try:
+    file = open("app.py")
     age = int(input("Age:"))
     exfactor = 10 / age
 except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown")
+finally:
+    file.close()
 
