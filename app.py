2a45b62d39e65147da9f27b507b069f6c007285b
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-26 08:20:50 +0300

    Exceptions handling

--- app.py
+++ app.py
@@ -2,4 +2,14 @@
 
 # print(numbers[3])
 
-age = int(input("Age:"))
+
+try:
+    age = int(input("Age:"))
+except ValueError as ex:
+    print("You didn't enter a valid age.")
+    print(ex)
+    print(type(ex))
+else:
+    print("No exceptions were thrown")
+print("Execution continues.")
+
