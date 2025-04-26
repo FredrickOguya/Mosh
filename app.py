e48a4b55e6ee20e2b82b33121bef4bff2e051208
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-26 19:30:30 +0300

    Classes

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-def calculate_xfactor(age):
-    if age <= 0:
-        raise ValueError("Age cannot be 0 or less.")
-    return 10/ age
+# class: blueprint for creating new objects
+# object: instance of a class
 
-try:
-    calculate_xfactor(-1)
-except ValueError as error:
-    print(error)
+# class: Human
+# objects: John, Mary, Jack
