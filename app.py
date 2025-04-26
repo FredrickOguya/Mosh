21b4152d9dcdada8df87891fda0ae91f6b66e1c7
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-26 08:30:50 +0300

    The with statement

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-
-
 try:
-    file = open("app.py")
+    with open("app.py") as file:
+        print("File opened.")
     age = int(input("Age:"))
     exfactor = 10 / age
 except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown")
-finally:
-    file.close()
+
 
