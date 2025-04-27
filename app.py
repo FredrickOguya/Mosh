7df50d932f93a18f3a9255f70ab2a76b5d754261
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-27 18:00:45 +0300

    Class vs instant Methods

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 class Point:
-    default_color = "red"
-
     def __init__(self, x, y ):
         self.x = x
         self.y = y
+
+    @classmethod
+    def zero(cls):
+        return cls(0, 0)
 
 
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
 
-Point.default_color = "yellow"
-
-point = Point(1,2)
-print(point.default_color)
-point.z = 10
+point = Point(0, 0)
+point = Point.zero()
 point.draw()
 
-another = Point(3,4)
-another.draw()
-print(another.default_color)
