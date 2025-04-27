7b813824dee32a72a746d2a07d11448b1085b70a
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-27 07:24:29 +0300

    Supporting Arithmetic Operations

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ class Point:
     def __init__(self, x, y ):
         self.x = x
         self.y = y
+    
+    def __add__(self,other):
+        return Point(self.x + other.x , self.y + other.y)
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
 
-    def __gt__(self, other):
-        return self.x > other.x and self.y > other.y
-
-point = Point(1, 2)
+point = Point(10, 20)
 other = Point(1, 2)
-print(point > other)
+combined = point + other
+print(combined.x)
