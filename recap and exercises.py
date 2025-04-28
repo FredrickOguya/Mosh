5402946e9f1015df13f6d1d5e64aa2cf4a226aba
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-28 08:18:20 +0300

    Class variable

--- recap and exercises.py
+++ recap and exercises.py
@@ -1,12 +1,12 @@
-# Instance Variables
-
-
-
+#Class variable
 class Dog:
+    species = "Canis familiaris"  # class variable
+
     def __init__(self, name):
         self.name = name  # instance variable
+
 dog1 = Dog("Buddy")
 dog2 = Dog("Max")
 
-print(dog1.name)  # Buddy
-print(dog2.name)  # Max
+print(dog1.species)  # Canis familiaris
+print(dog2.species)  # Canis familiaris
