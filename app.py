1cced73541fa5882bff3009d201b6e7976b2dd3a
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-28 16:00:40 +0300

    Multi-level Inheritance

--- app.py
+++ app.py
@@ -1,33 +1,10 @@
 class Animal:
-    def __init__(self):
-        self.age = 1
-
     def eat(self):
         print("eat")
 
+class Bird(Animal):
+    def fly(self):
+        print("fly")
 
-# Animal: Parent, Base
-# Mammal: Child, Sub
-class Mammal(Animal):
-    def __init__(self):
-        print("Animal Constructor")
-        print("Mammal constructor")
-        self.weight = 2
-        super().__init__()
-
-
-    def walk(self):
-        print("walk")
-
-
-class Fish(Animal):
-    def eat(self):
-        print("eat")
-     
-    def swim(self):
-        print("swim")
-
-m = Mammal()
-
-print(m.age)
-print(m.weight)
+class Chicken(Bird):
+    pass
