dca77f753765febbd2f3d866b3df696af80498e0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-28 08:04:20 +0300

    methods

--- recap and exercises.py
+++ recap and exercises.py
@@ -2,8 +2,11 @@ class Book:
     def __init__(self,title,author):
         self.title = title
         self.author = author
+    def display_info(self):
+        print(f"'{self.title}' by {self.author}")
 
 book1 = Book("The Alchemist", "Paulo Coelho")
 
 print("Title: ", book1.title)
 print("Author: ", book1.author)
+book1.display_info()
