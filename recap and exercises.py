f6a14b4c65eae38cea0252e0e777e367fd89a085
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-28 23:18:30 +0300

    Adding two movie duration

--- recap and exercises.py
+++ recap and exercises.py
@@ -1,16 +1,12 @@
-class Point:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+class Movie:
+    def __init__(self, title, duration):
+        self.title = title
+        self.duration = duration
 
     def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
+        return self.duration + other.duration
     
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-    
-p1 = Point(2,3)
-p2 = Point(4,1)
-result = p1 + p2
+m1 = Movie("Inception", 148)
+m2 = Movie("Intersteallar", 169)
 
-print(result)
+print(m1 + m2)
