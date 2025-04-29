a7c26b781c105d3fe74737ce53fca377b863fa1c
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-29 10:41:54 +0300

    Multilevel Inheritance

--- recap and exercises.py
+++ recap and exercises.py
@@ -2,16 +2,23 @@ class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
-    
     def introduce(self):
         print(f"My name is {self.name}, I'm {self.age} years old.")
 
 class Student(Person):
-    
-    def __init__(self,name, age, school):
+    def __init__(self,name,age, school):
         super().__init__(name,age)
         self.school = school
     
     def introduce(self):
         super().introduce()
         print(f"I study at {self.school}")
+
+class GraduateStudent(Student):
+    def __init__(self,name,age,school,course):
+        super().__init__(name,age,school)
+        self.course = course
+
+    def introduce(self):
+        super().introduce()
+        print(f"I am persuing a {self.course}.")
