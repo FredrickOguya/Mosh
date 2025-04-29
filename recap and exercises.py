0a3af2fd3f06030bdc7ff2abeda3226fbc5a2b34
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-29 09:05:30 +0300

    Private Members in Python

--- recap and exercises.py
+++ recap and exercises.py
@@ -1,23 +1,19 @@
-class Playlist:
-    def __init__(self):
-        self.songs = []
+class Student:
+    def __init__(self,name):
+        self.name = name
+        self.__grades = []
 
-    def add_songs(self,title):
-        self.songs.append(title)
+    def add_grade(self,grade):
+        self.__grades.append(grade)
+    
+    def get_average(self):
+        if self.__grades:
+            return sum(self.__grades) / len(self.__grades)
+        return 0
+    
+s = Student("Amina")
+s.add_grade(85)
+s.add_grade(90)
+s.add_grade(95)
 
-    def __getitem__(self,index):
-        return self.songs[index]
-    
-    def __setitem__(self,index,value):
-        self.songs[index] = value
-    
-    def __len__(self):
-        return len(self.songs)
-    
-playlist = Playlist()
-playlist.add_songs("Song A")
-playlist.add_songs("Song B")
-print(playlist[0])
-playlist[1] = "song X"
-print(playlist[1])
-print(len(playlist))
+print(s.get_average())
