747d558a352b9fa99cd603eea098d3da170e4a87
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-04-30 10:37:00 +0300

    A Good Example of Inheritance

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-class Flyer:
-    def fly(self):
-        pass
-
-class Swimmer:
-    def swim(self):
-        pass
-
-class FlyingFish(Flyer, Swimmer):
+class InvalidOperationError(Exception):
     pass
 
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is already open")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise InvalidOperationError("Stream is already close")
+        self.opened = False
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading data from a file")
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading data from a network")
