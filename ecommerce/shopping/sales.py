6fc68ad83f4c2e14d0002d24c4dde106880e6722
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-01 07:21:00 +0300

    Executing Modules as scripts

--- ecommerce/shopping/sales.py
+++ ecommerce/shopping/sales.py
@@ -1,11 +1,9 @@
-from ecommerce.customer import contact
-from ..customer import contact
-
-contact.contact_customer()
-
 def calc_tax():
     pass
 
 def calc_shipping():
     pass
 
+if __name__ == "__name__":
+    print("sales started")
+    calc_tax
