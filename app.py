8ea095b5b4a958fa15b54592135b6abad70dd14b
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-02 06:35:28 +0300

    Working with files

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from pathlib import Path
+from time import ctime
+import shutil
 
-# Path("/fredrick/videos")
-path = Path("ecommerce/__init__.py")
-# Path() /"ecommerce" / "__init__.py"
-# Path.home()
+source = Path("ecommerce/__init__.py")
+target = Path()/ "__init__.py"
 
-path.exists()
-path.is_file()
-path.is_dir()
-print(path.name)
-print(path.stem)
-print(path.suffix)
-print(path.parent)
-path = path.with_suffix(".txt")
-print(path.absolute())
+target.write_text(source.read_text())
+
+# path.exists()
+# path.rename("init.txt")
+# path.unlink()
+print(ctime(path.stat().st_ctime))
+
+# print(path.read_text())
+# path.write_text("...")
+# path.write_bytes()
