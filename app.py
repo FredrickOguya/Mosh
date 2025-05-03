0b30354b2b15382712039f014de0787cf44281b0
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-03 06:12:39 +0300

    Opening the Browser

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import random
-import string
+import webbrowser
 
-print(random.random())
-print(random.randint(1,10))
-print(random.choice([1, 2, 3, 4]))
-print(random.choices([1, 2, 3, 4], k=2))
-print("".join(random.choices(string.ascii_letters+string.digits, k=4)))
-
-numbers = [1, 2, 3, 4]
-random.shuffle(numbers)
-print(numbers)
+print("Deployment completed")
+webbrowser.open("http://google.com")
