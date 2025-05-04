947f28f9fc096076916daf54c07d7f435b4f3741
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-04 23:16:34 +0300

    Create a zip file and adding files

--- Recap and Exercises/Python Standard Library/recap.py
+++ Recap and Exercises/Python Standard Library/recap.py
@@ -1,16 +1,5 @@
-from pathlib import Path
+import zipfile
 
-base = Path('projects/demo_project')
-base.mkdir(parents=True,exist_ok=True)
-
-notes = base/'notes.txt'
-notes.write_text("This is a note")
-
-readme = base / 'readme.md'
-readme.write_text("#Demo Project")
-
-for items in base.iterdir():
-    print(items.resolve())
-
-readme.rename(base / 'README.md')
-notes.rename(base.parent / 'notes.txt')
+with zipfile.ZipFile('archive.zip', 'w') as zf:
+    zf.write('projects/notes.txt')
+    zf.write('projects/demo_project/README.md')
