f70734e9f91b99a97047d47184ee0cd0a5e78878
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2025-05-04 11:40:00 +0300

    writing to csv

--- Recap and Exercises/Python Standard Library/recap.py
+++ Recap and Exercises/Python Standard Library/recap.py
@@ -1,18 +1,7 @@
-# 💻 Your Practice Task
-# ✅ Write a script that:
-# 1️⃣ Creates a zip file called demo_project.zip
-# 2️⃣ Adds both projects/notes.txt and projects/demo_project/README.md
-# 3️⃣ Lists the contents of the zip
-# 4️⃣ Extracts the zip to a folder called unzipped_demo
+import csv
 
-import zipfile
-
-with zipfile.ZipFile('demo_project.zip','w') as zf:
-    zf.write('projects/notes.txt')
-    zf.write('projects/demo_project/README.md')
-
-with zipfile.ZipFile('demo_project.zip') as rf:
-    print(rf.namelist())
-
-with zipfile.Zipfile('demo_project.zip') as ef:
-    ef.extratractall('unzipped_demo')
+with open('people.csv', 'w', newline ='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Name','Age'])
+    writer.writerow(['Alice',30])
+    writer.writerow(['Bob', 25])
